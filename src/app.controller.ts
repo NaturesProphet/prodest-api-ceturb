4d1127ac153dd85dd86dcd4511ff2d8b18973967
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-07-30 15:50:01 -0300

    docs(Swagger): documentação com swagger na rota root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Get, Controller } from "@nestjs/common";
+import { Get, Controller, Res } from "@nestjs/common";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor( private readonly appService: AppService ) { }
+  /*
+    @Get()
+    root(): string {
+      return this.appService.root();
+    }
+    */
   @Get()
-  root(): string {
-    return this.appService.root();
+  root ( @Res() res ) {
+    res.redirect( '/api' );
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "app.module";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap () {
+  const app = await NestFactory.create( AppModule );
+  const options = new DocumentBuilder()
+    .setTitle( 'api-ceturb' )
+    .setDescription( 'implementação de casos de uso na api ca ceturb' )
+    .setVersion( '1.0' )
+    .addTag( 'nestjs' )
+    .build();
+  const document = SwaggerModule.createDocument( app, options );
+  SwaggerModule.setup( 'api', app, document );
+
+
+
+
+  await app.listen( 3000 );
 }
 bootstrap();
