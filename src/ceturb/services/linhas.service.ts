595901c4922b399c868724a5fa2b75cc5e8a367d
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-08-08 23:46:18 -0300

    fix(Linhas): Serviço re-re-fatorado com o novo request

--- src/ceturb/services/linhas.service.ts
+++ src/ceturb/services/linhas.service.ts
@@ -8,10 +8,6 @@ export class LinhasService {
     private readonly url: string = "https://gvbus.geocontrol.com.br/pontual-api-web/listarLinhas";
 
     public async retornar_linhas () {
-        let resp: Array<Linha> = await request.get( this.url, { json: true } );
-        for ( let index = 0; index < resp.length; index++ ) {
-            this.linhas.push( resp[ index ] );
-        }
-        return this.linhas;
+        return await request.get( this.url, { json: true } );
     }
 }
