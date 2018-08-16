3baac42241119d0e33dbfc353f2b8317bcf23cb9
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-08-16 14:17:32 -0300

    test(ponto): e2e de pontos ok

---
+++ test/pontos_de_paradae2e-spec.ts
@@ -0,0 +1,114 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { Test, TestingModule } from "@nestjs/testing";
+const feature = loadFeature( "./test/features/pontos_de_parada.feature" );
+import request from "supertest";
+
+import { INestApplication, HttpModule } from "@nestjs/common";
+import { AppModule } from "../src/app.module";
+import { PontoService } from '../src/ceturb/services/ponto.service'
+import { InformationNotFound } from "../src/ceturb/models/exception/InformationNotFound";
+jest.mock( '../src/ceturb/services/ponto.service' );
+
+let pontos: any;
+let resposta: any;
+
+defineFeature( feature, test => {
+  let module: TestingModule;
+  let app: INestApplication;
+
+  beforeAll( async () => {
+    module = await Test.createTestingModule( {
+      imports: [ HttpModule, AppModule ]
+    } ).compile();
+    app = module.createNestApplication();
+    await app.init();
+  } );
+
+  test( "Retornar todos os pontos ativos registrados.", ( {
+    given,
+    when,
+    then
+  } ) => {
+    given( "que existam pontos de parada registrados.", async () => {
+      PontoService.prototype.retornar_pontos = jest.fn().mockImplementationOnce( () => {
+        console.log( 'Mock da linha 34' );
+
+        let obj1 = {
+          "id": 5281,
+          "codigo": "000025",
+          "municipio": "Vila Velha",
+          "logradouro": "Rua Paulo Vinhas",
+          "referencia": "",
+          "longitude": -40.3350331363673,
+          "latitude": -20.447214118107,
+          "azimute": -68,
+          "terminal": false
+        }
+
+        let obj2 = {
+          "id": 404,
+          "codigo": "110001",
+          "municipio": "Vitoria",
+          "logradouro": "Avenida Paulino Muller",
+          "referencia": "Procuradoria da Republica do Espírito Santo - MPF",
+          "longitude": -40.3196039018834,
+          "latitude": -20.3170426845619,
+          "azimute": -14,
+          "terminal": false
+        }
+
+        let obj3 = {
+          "id": 481,
+          "codigo": "110002",
+          "municipio": "Vitoria",
+          "logradouro": "Avenida Paulino Muller",
+          "referencia": "Procuradoria da República do Espírito Santo - MPF",
+          "longitude": -40.3198290398441,
+          "latitude": -20.3164543118005,
+          "azimute": 170,
+          "terminal": false
+        }
+
+        return [ obj1, obj2, obj3 ];
+
+      } );
+      resposta = await request( app.getHttpServer() ).get( "/pontos" );
+      expect( resposta.status || resposta.body.status ).toBe( 200 );
+    } );
+
+    when( "o usuário solicitar as informações sobre os pontos.", async () => {
+      pontos = JSON.parse( JSON.stringify( resposta.body ) );
+    } );
+
+    then( "o sistema retorna todos os pontos ativos.", () => {
+      expect( pontos.length ).toBeGreaterThan( 0 );
+    } );
+  } );
+
+
+  test( "Não existem pontos ativos registrados.", ( { given, when, then } ) => {
+
+    given( "não existem pontos registrados", async () => {
+      PontoService.prototype.retornar_pontos = jest.fn().mockImplementationOnce( () => {
+        console.log( 'Mock da linha 51' );
+        return new InformationNotFound( "nenhum registro encontrado" );
+      } );
+      resposta = await request( app.getHttpServer() ).get( "/pontos" );
+      expect( resposta.body.status ).toBe( 204 );
+    } );
+
+    when( "o usuário solicitar as informações sobre os pontos.", async () => {
+      pontos = JSON.parse( JSON.stringify( resposta.body ) );
+    } );
+
+    then( "o sistema retorna uma mensagem informando que não há registros", () => {
+      expect( resposta.body.message ).toBe( "nenhum registro encontrado" );
+    } );
+  } );
+
+
+  afterAll( async () => {
+    await app.close();
+  } );
+
+} );
