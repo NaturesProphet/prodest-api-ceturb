599846ce3d4f2bb323647f28dd521343670a0ec3
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-08-29 17:41:01 -0300

    feat(viagens): endpoint para buscar viagens dos próximos 7 dias

--- src/ceturb/controllers/viagens.controller.ts
+++ src/ceturb/controllers/viagens.controller.ts
@@ -24,4 +24,21 @@ export class ViagensController {
                 .send( new InformationNotFound( "Não há registros" ) );
         }
     }
+
+    @Get( '7dias' )
+    @ApiOperation( { title: 'lista as viagens existentes para os próximos 7 dias' } )
+    @ApiResponse( { status: 200, description: 'Viagens encontradas' } )
+    @ApiResponse( { status: 204, description: 'Viagens não encontradas.' } )
+    public async lista_viagens_7dias ( @Res() res ) {
+        try {
+            res
+                .status( HttpStatus.OK )
+                .send( await this.service.retornar_viagens_7dias() );
+        } catch ( err ) {
+            res
+                .status( HttpStatus.NO_CONTENT )
+                .send( new InformationNotFound( "Não há registros" ) );
+        }
+    }
+
 }
