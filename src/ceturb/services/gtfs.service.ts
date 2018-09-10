3b0441bbd448d3edc672c57b563c89fb0a7817ec
Matheus Barbosa <matheus.barbosa@example.com>
2018-09-10 15:27:21 -0300

    feat(minio): Finalizacao das rotas do busca do minio

--- src/ceturb/services/gtfs.service.ts
+++ src/ceturb/services/gtfs.service.ts
@@ -1,28 +1,73 @@
 import { Injectable } from '@nestjs/common';
-import * as request from 'request-promise';
+import { Gtfs } from '../models/gtfs.model.Dto';
 import * as shell from 'shelljs';
 
 @Injectable()
 export class GtfsService {
-    private files: String[][];
-    
-    public async listAll () {
-        let output = shell.exec('./mc ls minio/gtfs').stdout;
-        /*
-        let i:number = 0
-        while(output.length > i){
-            let fileInfo: String[];
-            console.log("Testando0")
-            while( output[i] != "\\" ){
-                fileInfo.push(output[i]);
-                i++
-                console.log("TestandoX")
+    private files: Gtfs[] = [];
+    private readonly minioAddress = 'http://127.0.0.1:9000/gtfs';
+
+    constructor(){  //Cria array de GTFS que já contem todas as informações
+        let out = shell.exec('./mc ls minio/gtfs').stdout;
+        let results = out.split("\n");
+
+        results.pop(); // Retira a string vazia criada no split
+        
+        results.map(line => {
+            let result = line.split('-');
+            let split1 = result[2].split(" "); // Separacao do dia e da hora
+            let split2 = result[3].split(" "); // Separacao do tamanho
+            result[0] = result[0].substring(1); // Retirada do colchetes
+            let url = this.minioAddress + '/' + split2[split2.length-1];  
+            
+            let gtfs = {
+                year : result[0],
+                month : result[1],
+                day : split1[0],
+                hour : split1[1],
+                size : split2[split2.length-2],
+                filename : split2[split2.length-1],
+                url: url
             }
-            i+=2
-            this.files.push(fileInfo);
-            console.log("Testando1")
-        }*/             
-        return JSON.stringify(output)
+            this.files.push(gtfs);
+        });
+    }
+
+    public async getAll () {
+        return this.files;
+    }
+
+    public async getByYear(year: String){
+        let files: Gtfs[] = [];
+
+        this.files.map(line => {
+            if(line.year == year){
+                files.push(line);
+            }
+        })
+        return files;
+    }
+
+    public async getByYearMonth(year: String, month: String){
+        let files: Gtfs[] = [];
+
+        this.files.map(line => {
+            if(line.year == year && line.month == month){
+                files.push(line);
+            }
+        })
+        return files;
+    }
+
+    public async getByYearMonthDay(year: String, month: String, day: String){
+        let files: Gtfs[] = [];
+
+        this.files.map(line => {
+            if(line.year == year && line.month == month && line.day == day){
+                files.push(line);
+            }
+        })
+        return files;
     }
 }
 
---
+++ test/lista_gtfs.e2e-spec.ts
@@ -0,0 +1,73 @@
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { Test, TestingModule } from "@nestjs/testing";
+const feature = loadFeature( "./test/features/buscaGtfs.feature" );
+import request from "supertest";
+
+import { INestApplication, HttpModule, HttpStatus } from "@nestjs/common";
+import { AppModule } from "../src/app.module";
+import { GtfsService } from "../src/ceturb/services/gtfs.service";
+import { InformationNotFound } from "../src/ceturb/models/exception/InformationNotFound";
+jest.mock( "../src/app.module" );
+jest.mock( '../src/ceturb/services/gtfs.service' );
+
+defineFeature( feature, test => {
+  let module: TestingModule;
+  let app: INestApplication;
+  let gtfs;
+  let resposta;
+
+  beforeAll( async () => {
+    module = await Test.createTestingModule( {
+      imports: [ HttpModule, AppModule ]
+    } ).compile();
+    app = module.createNestApplication();
+    await app.init();
+  } );
+
+
+
+  test( "Existem arquivos GTFS registrados", ( {
+    given,
+    when,
+    then
+  } ) => {
+
+    given( "Eu quero saber as informações dos arquivos GTFS criados", async () => {
+        resposta = await request( app.getHttpServer() ).get( `/gtfs` );
+    } );
+
+    when( "eu pesquisar", async () => {
+      //Pesquisa feita em cima
+    } );
+    then( "recebo as informações", () => {
+      expect( resposta.length ).toBeGreaterThan( 0 );
+    } );
+  } );
+
+  test( "Não existem arquivos GTFS registrados", ( {
+    given,
+    when,
+    then
+  } ) => {
+    given( "Eu quero saber as informações dos horarios de uma linha", async () => {
+        GtfsService.prototype.getAll = jest.fn().mockImplementationOnce( () => {
+            return new InformationNotFound( "nenhum registro encontrado" );
+          } );
+          resposta = await request( app.getHttpServer() ).get( '/gtfs' );
+    } );
+
+    when( "eu pesquisar", async () => {
+      //pesquisa ja feita acima
+    } );
+
+    then( "recebo uma mensagem informando que não há arquivos", () => {
+      expect( resposta.message ).toBe( "Não há arquivos registrados" );
+    } );
+  } );
+
+
+  afterAll( async () => {
+    await app.close();
+  } );
+
+} );
