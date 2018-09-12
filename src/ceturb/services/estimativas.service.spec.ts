1ac7e33588ffe0892ed8122aa02c69c30d261cc6
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-09-12 00:00:17 -0300

    test(estimativas): testes unitarios do serviço de estimativas OK

--- src/ceturb/services/estimativas.service.spec.ts
+++ src/ceturb/services/estimativas.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EstimativasService } from './estimativas.service';
+import { OrigemEDestino } from '../models/origemEDestino.Dto';
+import { OrigemELinha } from '../models/origemELinha.Dto';
+jest.mock( './estimativas.service' );
 
 describe( 'EstimativasService', () => {
   let service: EstimativasService;
@@ -10,9 +13,27 @@ describe( 'EstimativasService', () => {
     service = module.get<EstimativasService>( EstimativasService );
   } );
 
-
-
-  it( 'should be defined', () => {
-    expect( service ).toBeDefined();
+  it( 'O retorno de estimativas por origem deve ser maior que 0', async () => {
+    let consulta = await service.ObterPorOrigem( 1 );
+    expect( consulta.estimativas.length ).toBeGreaterThan( 0 );
   } );
+
+
+  it( 'O retorno de estimativas por origem e destino devem ser maior que 0', async () => {
+    let query = new OrigemEDestino();
+    query.pontoDeDestinoId = 1;
+    query.pontoDeOrigemId = 1;
+    let consulta = await service.ObterPorOrigemEDestino( query );
+    expect( consulta.estimativas.length ).toBeGreaterThan( 0 );
+  } );
+
+
+  it( 'O retorno de estimativas por origem e Linha devem ser maior que 0', async () => {
+    let query = new OrigemELinha();
+    query.linhaId = 1;
+    query.pontoDeOrigemId = 1;
+    let consulta = await service.ObterPorOrigemELinha( query );
+    expect( consulta.estimativas.length ).toBeGreaterThan( 0 );
+  } );
+
 } );
