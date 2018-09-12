5a221acf7821461fb815ccf63da95facea2dab1c
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-09-12 00:04:31 -0300

    test(area): teste unitario de busca por area OK

--- src/ceturb/services/area.service.spec.ts
+++ src/ceturb/services/area.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AreaService } from './area.service';
+import { Envelope } from '../models/envelope.Dto';
+jest.mock( './area.service' );
 
-describe('AreaService', () => {
+describe( 'AreaService', () => {
   let service: AreaService;
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AreaService],
-    }).compile();
-    service = module.get<AreaService>(AreaService);
-  });
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  beforeAll( async () => {
+    const module: TestingModule = await Test.createTestingModule( {
+      providers: [ AreaService ],
+    } ).compile();
+    service = module.get<AreaService>( AreaService );
+  } );
+
+
+  it( 'O retorno de pontos de parada deve ser maior do que 0', async () => {
+    let query = new Envelope();
+    query.envelope = [ 1, 2, 3, 4 ];
+    let consulta = await service.BuscaPontosPorArea( query );
+    expect( consulta.pontosDeParada.length ).toBeGreaterThan( 0 );
+  } );
+
+
+} );
