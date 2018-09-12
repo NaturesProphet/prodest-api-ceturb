0ca80537112627fe9468f54229d37fcd563cc9a7
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-09-11 23:51:07 -0300

    chore(gtfs): testes unitarios do barbosa desativados

--- src/ceturb/services/estimativas.service.spec.ts
+++ src/ceturb/services/estimativas.service.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EstimativasService } from './estimativas.service';
 
-describe('EstimativasService', () => {
+describe( 'EstimativasService', () => {
   let service: EstimativasService;
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EstimativasService],
-    }).compile();
-    service = module.get<EstimativasService>(EstimativasService);
-  });
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  beforeAll( async () => {
+    const module: TestingModule = await Test.createTestingModule( {
+      providers: [ EstimativasService ],
+    } ).compile();
+    service = module.get<EstimativasService>( EstimativasService );
+  } );
+
+
+
+  it( 'should be defined', () => {
+    expect( service ).toBeDefined();
+  } );
+} );
