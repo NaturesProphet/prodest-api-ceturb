e818e0d375d7dd2f4e558aaf23e81442d764a931
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-09-27 16:32:49 -0300

    fix(gtfs_service): map --> forEach

--- src/ceturb/services/gtfs.service.ts
+++ src/ceturb/services/gtfs.service.ts
@@ -5,42 +5,42 @@ import { MinioService } from './minio.service'
 @Injectable()
 export class GtfsService {
     private files: Gtfs[] = [];
-    
 
-    constructor(private minioService:MinioService){ //Configura o minio 
+
+    constructor( private minioService: MinioService ) { //Configura o minio 
         this.minioService.config();
     }
 
-    private async ls(){ //Cria array de GTFS que já contem todas as informações
+    private async ls () { //Cria array de GTFS que já contem todas as informações
         this.files = [];
-        let out =  await this.minioService.ls();
-        let results = out.split("\n");
+        let out = await this.minioService.ls();
+        let results = out.split( "\n" );
 
         results.pop(); // Retira a string vazia criada no split
-        
-        results.map(line => {
-            let date = line.substring(1,11)
-            let calendar = date.split('-');
 
-            let hour = line.substring(12,20);
+        results.forEach( line => {
+            let date = line.substring( 1, 11 )
+            let calendar = date.split( '-' );
 
-            let result = line.substring(25);
-            let results = result.split(" ");
-            results = results.filter(n=>n);
+            let hour = line.substring( 12, 20 );
 
-            let url = this.minioService.getAddress() + '/gtfs/' + results[1];
+            let result = line.substring( 25 );
+            let results = result.split( " " );
+            results = results.filter( n => n );
+
+            let url = this.minioService.getAddress() + '/gtfs/' + results[ 1 ];
 
             let gtfs = {
-                year : calendar[0],
-                month : calendar[1],
-                day : calendar[2],
-                hour : hour,
-                size : results[0],
-                filename : results[1],
+                year: calendar[ 0 ],
+                month: calendar[ 1 ],
+                day: calendar[ 2 ],
+                hour: hour,
+                size: results[ 0 ],
+                filename: results[ 1 ],
                 url: url
             }
-            this.files.push(gtfs);
-        });
+            this.files.push( gtfs );
+        } );
     }
 
     public async getAll () {
@@ -48,39 +48,39 @@ export class GtfsService {
         return this.files;
     }
 
-    public async getByYear(year: String){
+    public async getByYear ( year: String ) {
         await this.ls();
         let files: Gtfs[] = [];
 
-        this.files.map(line => {
-            if(line.year == year){
-                files.push(line);
+        this.files.map( line => {
+            if ( line.year == year ) {
+                files.push( line );
             }
-        })
+        } )
         return files;
     }
 
-    public async getByYearMonth(year: String, month: String){
+    public async getByYearMonth ( year: String, month: String ) {
         await this.ls();
         let files: Gtfs[] = [];
 
-        this.files.map(line => {
-            if(line.year == year && line.month == month){
-                files.push(line);
+        this.files.map( line => {
+            if ( line.year == year && line.month == month ) {
+                files.push( line );
             }
-        })
+        } )
         return files;
     }
 
-    public async getByYearMonthDay(year: String, month: String, day: String){
+    public async getByYearMonthDay ( year: String, month: String, day: String ) {
         await this.ls();
         let files: Gtfs[] = [];
 
-        this.files.map(line => {
-            if(line.year == year && line.month == month && line.day == day){
-                files.push(line);
+        this.files.map( line => {
+            if ( line.year == year && line.month == month && line.day == day ) {
+                files.push( line );
             }
-        })
+        } )
         return files;
     }
 }
