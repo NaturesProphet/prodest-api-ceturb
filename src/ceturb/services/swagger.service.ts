f30c86b95a234f0d84950908121ed9c810766a29
Gary M. Silva <garymsilva@outlook.com>
2018-11-14 21:26:15 -0200

    fix(main.ts): criando arquivo swagger.json

--- src/ceturb/services/swagger.service.ts
+++ src/ceturb/services/swagger.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-const swagger = require( '../../../swagger.json' );
+const swagger = require( '../../swagger.json' );
 
 @Injectable()
 export class SwaggerService {
