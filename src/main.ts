4a3f5ce3937225b3895e8ab3339075777df49d05
Matheus Barbosa <matheuskleber@live.com>
2019-03-15 16:36:55 -0300

    chore(CORS): Configurando CORS com o nestjs

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Filtro } from "./commom/filter/Filtro";
 import { Endpoints } from './commom/configs/endpoints.config';
 
-import * as cors from 'cors'
-
 const raiz: string = new Endpoints().rotaRaiz;
 const pacote = require( '../package.json' );
 const fs = require( 'fs' );
 
 async function bootstrap () {
   const app = await NestFactory.create( AppModule );
-  app.use(cors());
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle( 'api-ceturb' )
     .setDescription( pacote.description )
